1f3b2006161ae464de5f1a1ec431a86a53dd54af
Julius Ngwu <juliusngwu@Juliuss-MacBook-Pro.local>
2019-06-05 23:21:48 +0100

    10th tutorial

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component{
     render() {
-        return <p>Hello World</p>
+        return (
+            <form className="store-selector">
+                <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="Store Name" defaultValue={ getFunName()}/>
+                <button type="submit">Visit Store =></button>
+            </form>
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
+import Router from './components/Router';
+import './css/style.css';
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
